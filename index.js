f0f5aa64df31fcfe40696ebcd76d6f482f1c8354
Sahrial Ardians <sahrialardians@gmail.com>
2021-06-24 22:39:08 +0700

    update code to get data product

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 var express = require("express");
 var app = express();
 
+var Products = require("./product.json");
+
 app.get('/', (req, res) => {
     res.send('Simple rest api');
 });
 
 app.listen(5000, () => {
  console.log("Server running on port 5000");
+});
+
+//GET all list
+app.get("/product/listall", (req, res) => {
+    res.json(Products);
+});
+
+//GET detail by id
+app.get("/product/:id", (req, res) => {
+    res.json(Products.find((product) => {
+        return req.params.id == product.id
+    }))  
 });
